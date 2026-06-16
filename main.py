98430ef3689268e95f77bd617985d24641914b19
RIYANSH JAIN <theriyanshjain1@gmail.com>
2026-06-16 15:43:01 +0530

    Implement document chunking pipeline

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from src.loader.pdf_loader import load_pdf
+from src.chunking.text_splitter import create_chunks
 
 docs = load_pdf("data/papers/NLP- MODULE 1.pptx.pdf")
 
+chunks = create_chunks(docs)
+
 print(f"Total Pages: {len(docs)}")
-print()
-print(docs[0].page_content[:1000])
-print(docs[0].metadata)
+print(f"Total Chunks: {len(chunks)}")
+
+print("\nFirst Chunk:\n")
+print(chunks[0].page_content[:500])
+
+print("\nMetadata:\n")
+print(chunks[0].metadata)
+print(len(chunks[0].page_content))
